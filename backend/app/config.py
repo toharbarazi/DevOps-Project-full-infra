f364ff62f02a59a8f8e3230ac69cb79886dc7484
toharbarazi <tohar.barazi@nitaznim.tech>
2025-09-14 18:51:13 +0300

    docker compose for frontend and backend

--- backend/app/config.py
+++ backend/app/config.py
@@ -20,9 +20,9 @@ ALLOWED_IMAGE_EXTENSIONS = ['jpg', 'png', 'jpeg']
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 
 CACHE_TYPE = "RedisCache"
-CACHE_REDIS_URL = "redis://localhost:6379/0"
+CACHE_REDIS_URL = "redis://redis:6379/0"
 CACHE_DEFAULT_TIMEOUT = 300
 DEBUG = False
 
-CELERY_BROKER_URL = "redis://localhost:6379/1"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/2"
+CELERY_BROKER_URL = "redis://redis:6379/1"
+CELERY_RESULT_BACKEND = "redis://redis:6379/2"
--- backend/main.py
+++ backend/main.py
@@ -123,6 +123,6 @@ api.add_resource(ExportAPI, '/api/export-posts-csv')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
+        app.run(host='0.0.0.0', debug=True)
 
 
