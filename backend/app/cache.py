920fc925bfcac395e415b614683ad10a4fb37d73
toharbarazi <tohar.barazi@nitaznim.tech>
2025-09-16 17:21:27 +0300

    new dir

--- backend/app/cache.py
+++
@@ -1,4 +0,0 @@
-from flask_caching import Cache
-from flask import current_app as app
-
-cache = Cache()
--- backend/app/security.py
+++
@@ -1,5 +0,0 @@
-from flask_security import Security, SQLAlchemyUserDatastore
-from .models import db, User, Role
-
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security()
