a69f79badcf234ef78b3b640e2892eba8a8a23e3
toharbarazi <tohar.barazi@nitaznim.tech>
2025-09-17 23:05:06 +0300

    terraform VPC EC2

--- Terraform/modules/EC2/main.tf
+++ Terraform/modules/EC2/main.tf
@@ -2,13 +2,57 @@
 # EC2 Instance
 resource "aws_instance" "k8s_node" {
   ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public_subnet.id
+  instance_type               = "t3.medium"
+  subnet_id                   = var.public_subnet_id
   vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
   key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
   tags = {
-    Name = "k8s-node"
+    Name = var.EC2_name
   }
 }
+
+# Security Group
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH, HTTP, HTTPS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
+
